f7ffeb7e49871cfc915f6637b7a26881bc810f2d
Ekaterina <kmatway@yandex.ru>
2022-12-22 00:07:06 +0500

    решение задачи 27

--- Z27/Program.cs
+++ Z27/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+Console.WriteLine("Введите число:  ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр = {GetColNum(num)}");
+
+int GetColNum(int number)
+{
+    int count = 0;
+    int sum = 0;
+   while (number > 0)
+   {
+   sum = sum + (number%10);
+   number = number/10;
+   
+   count++;
+   }
+   return sum;
+}
+
