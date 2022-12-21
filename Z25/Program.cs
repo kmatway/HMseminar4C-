87d586943c968eb9eaa7f722d5739c9de7f4ce46
Ekaterina <kmatway@yandex.ru>
2022-12-21 23:55:25 +0500

    решение задачи 25

---
+++ Z25/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число A:  ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B:  ");
+int b = int.Parse(Console.ReadLine());
+
+if(b > 0)
+{
+    Console.WriteLine($"Ответ = {GetExpo(a,b)}");
+}
+else
+{
+    Console.WriteLine("Число B не является натуральным");
+}
+
+int GetExpo(int numberA, int numberB)
+{
+    int result = 1;
+    int count = 1;
+    while (count <= numberB)
+    {
+        result = result * numberA;
+        count++;
+    }
+    return result;
+}
+
